492a87e1bf911fe8529c5dcf5471ea1ea69670f6
zio-podolsk <kuznetsov-max@ya.ru>
2024-04-27 11:27:36 +0300

    add rating

--- src/pages/todoList.vue
+++ src/pages/todoList.vue
@@ -1,3 +1,18 @@
 <template>
-  <q-card class="clock_card q-ma-md q-pa-xs flex flex-center"> <h5>Страница тудушка</h5></q-card>
+  <q-card class="clock_card q-ma-md q-pa-xs flex flex-center"
+    ><h5>Страница тудушка</h5>
+    <q-rating
+      class="q-pa-md"
+      v-model="ratingModel"
+      size="1.5em"
+      :max="7"
+      color="primary"
+    ></q-rating>
+  </q-card>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const ratingModel = ref(3);
+</script>
