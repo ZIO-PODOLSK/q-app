b78765fce2676a8c0be9a8f5f79cbe7f281e745e
zio-podolsk <kuznetsov-max@ya.ru>
2024-05-03 10:43:28 +0300

    update

--- src/pages/QuasarList.vue
+++ src/pages/QuasarList.vue
@@ -3,7 +3,7 @@
     <q-card class="clock_card q-ma-md q-pa-xs flex flex-center">
       <h6>Справочник по компонентам Quasar</h6></q-card
     >
-    <q-card>
+    <q-card class="clock_card q-ma-md q-pa-xs">
       <q-chip :class="{ square: selection }">Элемент chip</q-chip>
       <q-checkbox v-model="selection" val="red" label="rounded" color="red" />
       <q-chip color="primary" text-color="white" icon="event">
